71a9cf99903798904023be225745c7896e8b9a86
Genro75 <genro.ap@gmail.com>
2020-12-15 19:06:22 +0530

    Jquery

---
+++ res/Jss/script.js
@@ -0,0 +1,24 @@
+$(Document).read(
+    function(){
+        $('.js--section-features').waypoint(
+            function(direction){
+                if (direction == "down"){
+                    $('nav').addClass('sticky');
+                } else{
+                    $('nav').removeClass('sticky');
+                }
+            },{
+                offset:'60px'
+            }
+        );
+
+
+        /* ------reffered from waypoint-------
+        
+        var waypoints = $('#handler-first').waypoint(function(direction) {
+            notify(this.element.id + ' hit 25% from top of window') 
+            }, {
+            offset: '25%'
+        })*/
+    }
+);
